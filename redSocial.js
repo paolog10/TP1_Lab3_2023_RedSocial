142a3856856add69446cbf0f5a39ada0e7a93b8c
Paolo Gamarra <paologamarra10@gmail.com>
2023-04-29 19:59:34 -0300

    agregado comentarios

--- redSocial.js
+++ redSocial.js
@@ -6,25 +6,65 @@ function Iniciar() {
 
   let seguirPerfil = document.getElementById('btnSeguir');
   seguirPerfil.addEventListener("click", SeguirPerfil);
+
+  let agregarComentarios = document.getElementById('agregarComentario');
+  agregarComentarios.addEventListener("click", GenerarComentarios);
 }
 
 function MostrarNombre(evento) {
   evento.preventDefault(); // Evita que se recargue la página al enviar el formulario
-  alert("funciona") //despues eliminar
+  //alert("funciona") //despues eliminar
 }
 
 function SeguirPerfil() {
-  evento.preventDefault() // Evita que se recargue la página al enviar el formulario
+  //evento.preventDefault() // Evita que se recargue la página al enviar el formulario
 
   let seguirPerfil = document.getElementById('btnSeguir');
   seguirPerfil.addEventListener("click", Estado)
   
   function Estado() {
     if (seguirPerfil.textContent === 'Seguir') {
-      seguirPerfil.textContent = 'Siguiendo';
+      seguirPerfil.textContent = 'Dejar de seguir'; //La propiedad textContent para establecer el texto del párrafo
     } else {
       seguirPerfil.textContent = 'Seguir';
     }
   }
+}
+
+function DarLike(evento) {
+  evento.preventDefault() // Evita que se recargue la página al enviar el formulario
+  
+  let btnLike = document.getElementById('btnLike');
+  btnLike.addEventListener("click", SumarLike);
+  let contarLike = document.getElementById('contarLike');
+  let numerosLike = 200;
+
+  function SumarLike() {
+    numerosLike++;
+    contarLike.textContent = numerosLike; //La propiedad textContent para establecer el texto del párrafo
+  }
+};
+
+function GenerarComentarios(evento) {
+  evento.preventDefault();
+  
+  let inputUsuario = document.getElementById('txtUsuario').value;
+  let inputComentario = document.getElementById('inputComentario').value;
+  let listaComentarios = document.getElementById('ulMostrarComentarios');
+
+  //creo elemento 'li'
+  let nuevoComentario = document.createElement("li");
+
+  //imprimo el comentario
+  var contenido = document.createTextNode(inputUsuario + ': ' + inputComentario);
+  
+  listaComentarios.appendChild(nuevoComentario);
+  nuevoComentario.appendChild(contenido);
 
 }
+/*
+TODO
+-revisar Me gusta uso textContent
+-revisar Seguir uso textContent
+*/
+
